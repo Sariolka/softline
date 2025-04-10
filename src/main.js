132d51848424917105d26bbdf147860e034a3f0e
Sariolka <sariola@list.ru>
2025-04-10 22:00:29 +0300

    fix: delete plugin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './assets/styles.scss';
 
 import { createApp } from 'vue';
-import VueObserveVisibility from 'vue3-observe-visibility'
 import App from './App.vue';
 
-createApp(App).use(VueObserveVisibility).mount('#app');
+createApp(App).mount('#app');
